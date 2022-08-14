5b0265ebaedbe996a359522e8369654a7a5e0c96
Ramiro Castro <ramirocastrocavalcanti@outlook.com>
2022-08-14 14:57:32 -0300

    Refatorando a funcao que verifica o checkbox responsavel por sinalizar o aceite das informações, assim como o corringindo erros de lint da mesma

--- script.js
+++ script.js
@@ -16,21 +16,18 @@ document.querySelector('#textarea').addEventListener('keyup', () => {
   document.getElementById('counter').innerText = 500 - text.length;
 });
 
-//https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
-//desabilita o botão no início
-document.getElementById("submit-btn").disabled = true;
+// https://cursos.alura.com.br/forum/topico-habitar-desabilitar-botao-65202
+// desabilita o botão no início
+document.getElementById('submit-btn').disabled = true;
 
-//https://pt.stackoverflow.com/questions/307752/habilitar-desabilitar-um-bot%C3%A3o-se-pelo-menos-um-checkbox-for-marcado
-var checa = document.getElementsByName("toggle");
-var numElementos = checa.length;
-var bt = document.getElementById("submit-btn");
+// https://pt.stackoverflow.com/questions/307752/habilitar-desabilitar-um-bot%C3%A3o-se-pelo-menos-um-checkbox-for-marcado
+const checa = document.getElementsByName('toggle');
+const bt = document.getElementById('submit-btn');
 
-	checa[0].onclick = function(){
-	   // "input[name='toggle']:checked" conta os checkbox checados
-	   var cont = document.querySelectorAll("input[name='toggle']:checked").length;
-	   // ternário que verifica se há algum checado.
-	   // se não há, retorna 0 (false), logo desabilita o botão
-	   bt.disabled = cont ? false : true;
-	}
- 
-
+checa[0].onclick = function checkbox() {
+  // "input[name='toggle']:checked" conta os checkbox checados
+  const cont = document.querySelectorAll('input[name=\'toggle\']:checked').length;
+  // ternário que verifica se há algum checado.
+  // se não há, retorna 0 (false), logo desabilita o botão
+  bt.disabled = !cont;
+};
